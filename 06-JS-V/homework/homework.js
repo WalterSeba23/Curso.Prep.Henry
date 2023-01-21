2a1dc9e0fa67e722fd5afbcdbf387095c87f5c31
Sebastian Moreira <moreirawaltersebastian@gmail.com>
2023-01-21 17:42:07 -0300

    Homework JSV Resuelto

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -81,7 +81,7 @@ function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve:
   //Ej: "Juan, 22 años"
   Persona.prototype.datos = function () {
-    return this.nombre + ", " + this.edad + " dedos";
+    return this.nombre + ", " + this.edad + " años";
   };
 }
 
