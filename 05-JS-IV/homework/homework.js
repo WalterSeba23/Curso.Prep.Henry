ca105cc8699d4924dc71957bb80017187f4b6519
Sebastian Moreira <moreirawaltersebastian@gmail.com>
2023-01-21 17:34:11 -0300

    Homework JSV Resuelto

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -155,7 +155,7 @@ function agregarMetodoCalculoDescuento(producto) {
   producto.calcularPrecioDescuento = function () {
     return this.precio - this.precio * this.porcentajeDeDescuento;
   };
-  return property;
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
